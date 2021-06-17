b250f5793df3ce77ca211f97bb6f492f03a3f58f
Олег Бровченков <oleg.brovchenkov@gmail.com>
2021-06-17 09:15:10 +0300

    Spawner updated

--- Assets/scripts/Spawner.cs
+++ Assets/scripts/Spawner.cs
@@ -5,7 +5,7 @@ public class Spawner : MonoBehaviour
     // Canvas script;
 
     public Enemy[] enemies;
-    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject[] enemyPrefabs;
     [SerializeField] public float frequency; // frequency раз в секундку тик
     [SerializeField] public short mass; // Мобов за тик
     [SerializeField] Transform EnemiesHandler;
@@ -17,13 +17,21 @@ public class Spawner : MonoBehaviour
         frequency = 1 / frequency;
         InvokeRepeating("Spawn", frequency, frequency);
     }
+    short GetEnemyId()
+    {
+        short i = (short)Random.Range(0, 22);
+        //i = (short)(i == 21 ? 2 : i > 15 ? i = 1 : 0);
+        if (i == 21) return 2; // Large
+        if (i > 15) return 1;  // Medium;
+        else return 0;         // Small
+    }
     void Spawn()
     {
         for (short _ = 0; _ < mass; _++)
         {
             short index = GetFreeSlot();
             if (index == -1) return;
-            Enemy e = Instantiate(enemyPrefab, EnemiesHandler).GetComponent<Enemy>();
+            Enemy e = Instantiate(enemyPrefabs[GetEnemyId()], EnemiesHandler).GetComponent<Enemy>();
             float r = Random.Range(950, 1100);
             e.transform.localPosition = new Vector2(Mathf.Sin(Random.Range(-500, 500)) * r, Mathf.Cos(Random.Range(-500, 500)) * r);
             enemies[index] = e;
